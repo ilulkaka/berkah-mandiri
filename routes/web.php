12956aacc83dc395de99c07eaf182288c35a2238
ilulkaka <ilul.kaka@gmail.com>
2022-11-14 15:57:07 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', 'LoginController@login')->name('login');
+Route::post('loginaksi', 'LoginController@loginaksi')->name('loginaksi');
+Route::get('home', 'HomeController@index')
+    ->name('home')
+    ->middleware('auth');
+Route::get('logoutaksi', 'LoginController@logoutaksi')
+    ->name('logoutaksi')
+    ->middleware('auth');
